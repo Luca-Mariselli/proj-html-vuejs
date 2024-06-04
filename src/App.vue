e248202638578b919ce002ca091403ed555e1566
Luca-Mariselli <lucamariselli@gmail.com>
2024-06-04 13:17:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+// import AppHeader from './components/AppHeader.vue';
+// import AppMain from './components/AppMain.vue';
+// import AppFooter from './components/AppFooter.vue';
+import store from './data/store.js';
+
+export default {
+  components: {
+    // AppMain,
+  },
+  data() {
+    return {
+      store,
+
+    }
+  },
+  methods: {
+    getImage(percorso) {
+      return new URL(`./assets/img/${percorso}`, import.meta.url)
+    }
+  },
+  mounted() {
+    console.log(this.store.films[0].title)
+  }
+}
+</script>
+
+<template>
+
+
+
+  <div v-for="film, i in store.films">
+    {{ film.title }}
+    <div v-for="genere in film.genre">{{ genere }}</div>
+    <img :src="getImage(film.img_rec)" alt="">
+  </div>
+
+
+
+
+</template>
+
+<style scoped></style>
---
+++ src/data/store.js
@@ -0,0 +1,26 @@
+import {reactive} from 'vue';
+
+const iMieiDati = reactive({
+	films: [
+        {
+            title: `Hellboy`,
+            genre: [`Action`, `Drama`, `Superheroes` ],
+            img_rec: `1-rec.jpg`,
+            img_sq: `1-sq.jpg`,
+            release: ``,
+            category: ``,
+            views: ``,
+            video: ``,
+            viewFooter: true,
+            viewNewMovie: true,
+            viewLatestNews: true,
+        }
+        
+
+        
+        
+    ],
+	
+});
+
+export default iMieiDati;
